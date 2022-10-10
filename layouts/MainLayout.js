97e9f1764e519557d9f77cd8cc0c3d8bee64fa3a
Sithum <sithum.dashantha@gmail.com>
2022-10-10 23:45:30 +0530

    bottom tab navigator

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -1,14 +1,41 @@
-const { View } = require("react-native");
+const { View, Animated } = require("react-native");
+import { useRef } from "react";
 import { WHITE } from "../constants/colors";
+import { WIDTH } from "../constants/sizes";
 
-const MainLayout = () => (
-  <View
-    style={{
-      backgroundColor: WHITE,
-    }}
-  >
-    <Text>Main Layout</Text>
-  </View>
-);
+const MainLayout = () => {
+  const flatListRef = useRef();
+
+  function renderContent() {
+    return (
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <Animated.FlatList
+          ref={flatListRef}
+          horizontal
+          pagingEnabled
+          snapToAlignment="center"
+          snapToInterval={WIDTH}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+        ></Animated.FlatList>
+      </View>
+    );
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: WHITE,
+      }}
+    >
+      <Text>Main Layout</Text>
+    </View>
+  );
+};
 
 export default MainLayout;
